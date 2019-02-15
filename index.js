76a7acca13616a6ae6121eb1ff203f2f913c6b6c
yazmen Davis <yazmendavis94@gmail.com>
2019-02-15 02:05:35 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,31 @@
+var uppercase, lowercase
+ 
+function shout(string){
+  uppercase = string.toUpperCase();
+  string = uppercase;
+  return uppercase;
+}
+
+function whisper(string){
+  lowercase = string.toLowerCase();
+  string = lowercase;
+  return lowercase;
+}
+
+function logShout(string){
+  uppercase = string.toUpperCase();
+  string = uppercase;
+  console.log(uppercase);
+}
+
+function logWhisper(string){
+  lowercase = string.toLowerCase();
+  string = lowercase;
+  console.log(lowercase)
+}
+
+function sayHiToGrandMa(string){
+ if (
+   toLowerCase() === 
+   )
+}
